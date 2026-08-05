a984e03a0b5adf8064270ceafc17b290614cca38
elyamrh88-sof <elyamrh88@gmail.com>
2026-08-05 17:35:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Subscribe button on every webpage
+function subscribeUser() {
+  alert("Thank you for subscribing.");
+}
+
+// Gallery page buttons
+function addToCart() {
+  alert("Item added to the cart.");
+}
+
+function clearCart() {
+  alert("Cart cleared.");
+}
+
+function processOrder() {
+  alert("Thank you for your order.");
+}
+
+// Contact form button
+function submitMessage() {
+  alert("Thank you for your message.");
+}
